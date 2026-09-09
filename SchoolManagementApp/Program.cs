ff3d2b88ede0ef6ea86cdb541c9c94eb832190c2
sriram.kannan <sriram.kannan@PSILENL344>
2026-09-09 15:08:01 +0530

    Add project files.

---
+++ SchoolManagementApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    public static void Main(string[] args)
+    {        
+        string appName = "School Management Console App";
+        Console.WriteLine($"Welcome to the {appName} !!");        
+    }
+}

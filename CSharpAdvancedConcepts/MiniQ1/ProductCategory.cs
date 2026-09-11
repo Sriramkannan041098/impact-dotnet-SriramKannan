31fef7f8655ab948a55e78385028ac62dc7de9ed
Sriramkannan041098 <sriram.kannan@psiog.com>
2026-09-11 17:59:52 +0530

    Changes - 15  Final change

--- CSharpAdvancedConcepts/MiniQ1/ProductCategory.cs
+++ CSharpAdvancedConcepts/MiniQ1/ProductCategory.cs
@@ -16,12 +16,12 @@
     /*Since, class and enum are types they cant share similar names*/
     /*
      Type Declarations in C#:
-     Class	        Defines objects with data and behavior	class Employee { }
-     Struct	        Value type for small data structures	struct Coordinate { }
-     Record	        Represents data with value-based equality	record Address(string City);
-     Record Struct	Value-type version of a record	record struct Point(int X, int Y);
-     Enum	        Defines a set of named constants	enum Days { Monday, Tuesday }
+     Class	        Defines objects with data and behavior	            class Employee { }
+     Struct	        Value type for small data structures	            struct Coordinate { }
+     Record	        Represents data with value-based equality           record Address(string City);
+     Record Struct	Value-type version of a record	record struct       Point(int X, int Y);
+     Enum	        Defines a set of named constants	                enum Days { Monday, Tuesday }
      Interface	    Defines a contract that classes/structs implement	interface IRepository { }
-     Delegate	    Represents a reference to a method	delegate void MyHandler();
+     Delegate	    Represents a reference to a method	delegate void   MyHandler();
      */
 }
